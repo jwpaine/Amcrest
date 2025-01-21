747f03f34570210ba8923d1d1c9a2371e8b4bb2e
tsunami <tsunami@beast.beast>
2025-01-20 20:09:35 -0500

    channel sum

--- run.go
+++ run.go
@@ -51,7 +51,7 @@ func main() {
 
 	frame_changed := []byte{}
 
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 100; i++ {
 		// filename := fmt.Sprintf("frame_%d.jpg", i)
 		// saveFrame(camera, filename)
 
@@ -95,16 +95,36 @@ func compareImages(img1, img2 image.Image) {
 		fmt.Println("same bounds")
 	}
 
-	// Compare pictures by calculating histogram for each color value?
+	fmt.Println(img1.Bounds())
+
+	// Compare pictures by calculating the difference in channel averages between images
 	bounds := img1.Bounds()
+
+	colors_1 := [4]uint32{0, 0, 0, 0}
+	colors_2 := [4]uint32{0, 0, 0, 0}
+
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			colors := img1.At(x, y)
-			r1, g1, b1, a1 := colors.RGBA()
+			colors1 := img1.At(x, y)
+			r1, g1, b1, a1 := colors1.RGBA()
+			// normalize the RGBA() values back to the 8-bit range (0–255) by dividing each channel value by 257
+			colors_1[0] += r1 / 257
+			colors_1[1] += g1 / 257
+			colors_1[2] += b1 / 257
+			colors_1[3] += a1 / 257
 
+			colors2 := img2.At(x, y)
+			r2, g2, b2, a2 := colors2.RGBA()
+			colors_2[0] += r2 / 257
+			colors_2[1] += g2 / 257
+			colors_2[2] += b2 / 257
+			colors_2[3] += a2 / 257
 		}
 	}
 
-	// // Images are identical
-	// return true
+	p := uint32(bounds.Max.X * bounds.Max.Y)
+
+	fmt.Printf("image1 channel sum: %d %d %d %d\n", colors_1[0]/p, colors_1[1]/p, colors_1[2]/p, colors_1[3]/p)
+	fmt.Printf("image2 channel sum: %d %d %d %d\n", colors_2[0]/p, colors_2[1]/p, colors_2[2]/p, colors_2[3]/p)
+
 }
