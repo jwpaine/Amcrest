b2b433f0693540d508dfb0609eb521f6f4ff9877
tsunami <tsunami@beast.beast>
2025-01-20 22:25:45 -0500

    Calculate change %, write image to disk

--- run.go
+++ run.go
@@ -8,25 +8,21 @@ import (
 	"image/jpeg"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 )
 
-func saveFrame(camera *amcrest.Camera, filename string) {
-	frame, err := camera.GetSnapshot()
+func writeFrame(image []byte) {
 
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	file, err := os.Create(filename)
+	file, err := os.Create(time.Now().String() + ".jpg")
 	if err != nil {
 		log.Fatalf("Error creating file: %v", err)
 	}
 	defer file.Close()
 
 	// Write the []byte directly to the file
-	_, err = file.Write(frame)
+	_, err = file.Write(image)
 	if err != nil {
 		log.Fatalf("Error saving image to file: %v", err)
 	}
@@ -48,83 +44,79 @@ func main() {
 	fmt.Printf("%s %s %s\n", uri, loginname, password)
 
 	camera := amcrest.Init(uri, loginname, password)
+	frame_saved := []byte{}
 
-	frame_changed := []byte{}
+	threshold := 2000
+	maxPercent := 5.5
 
 	for i := 0; i < 100; i++ {
-		// filename := fmt.Sprintf("frame_%d.jpg", i)
-		// saveFrame(camera, filename)
 
-		frame, err := camera.GetSnapshot()
+		frame_current, err := camera.GetSnapshot()
 
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		if len(frame_changed) == 0 {
+		if len(frame_saved) == 0 {
 			fmt.Println("Saving first frame")
-			frame_changed = frame
+			frame_saved = frame_current
 			continue
 		}
 
 		// Decode as JPEG
-		saved, err := jpeg.Decode(bytes.NewReader(frame_changed))
+		saved, err := jpeg.Decode(bytes.NewReader(frame_saved))
 		if err != nil {
 			log.Fatalf("Error decoding saved JPEG image: %v", err)
 		}
-		current, err := jpeg.Decode(bytes.NewReader(frame))
+		current, err := jpeg.Decode(bytes.NewReader(frame_current))
 		if err != nil {
 			log.Fatalf("Error decoding current JPEG image: %v", err)
 		}
 
-		fmt.Println("Images successfully decoded as JPEG")
+		if Change(saved, current, uint32(threshold), float64(maxPercent)) {
+			fmt.Printf("Images differ > %f percent\n", maxPercent)
+			writeFrame(frame_current)
+			frame_saved = frame_current
 
-		// bounds := img.Bounds()
-		// fmt.Printf("Image dimensions: %dx%d\n", bounds.Dx(), bounds.Dy())
-		compareImages(saved, current)
+		}
 
 	}
 
 }
 
-func compareImages(img1, img2 image.Image) {
+func Change(img1, img2 image.Image, threshold uint32, maxPercent float64) bool {
 
 	if img1.Bounds() != img2.Bounds() {
 		fmt.Println("not same bounds")
-	} else {
-		fmt.Println("same bounds")
+		return false
 	}
-
-	fmt.Println(img1.Bounds())
 
 	// Compare pictures by calculating the difference in channel averages between images
 	bounds := img1.Bounds()
-
-	colors_1 := [4]uint32{0, 0, 0, 0}
-	colors_2 := [4]uint32{0, 0, 0, 0}
+	changedPixels := 0
+	totalPixels := (bounds.Max.X - bounds.Min.X) * (bounds.Max.Y - bounds.Min.Y)
 
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			colors1 := img1.At(x, y)
-			r1, g1, b1, a1 := colors1.RGBA()
-			// normalize the RGBA() values back to the 8-bit range (0–255) by dividing each channel value by 257
-			colors_1[0] += r1 / 257
-			colors_1[1] += g1 / 257
-			colors_1[2] += b1 / 257
-			colors_1[3] += a1 / 257
 
-			colors2 := img2.At(x, y)
-			r2, g2, b2, a2 := colors2.RGBA()
-			colors_2[0] += r2 / 257
-			colors_2[1] += g2 / 257
-			colors_2[2] += b2 / 257
-			colors_2[3] += a2 / 257
+			r1, g1, b1, a1 := img1.At(x, y).RGBA()
+			r2, g2, b2, a2 := img2.At(x, y).RGBA()
+
+			// Check if any channel difference exceeds the threshold
+			if absDiff(r1, r2) > threshold || absDiff(g1, g2) > threshold || absDiff(b1, b2) > threshold || absDiff(a1, a2) > threshold {
+				changedPixels++
+			}
 		}
 	}
+	percentageChanged := float64(changedPixels) / float64(totalPixels) * 100
+	fmt.Printf("Percentage changed: %f\n", percentageChanged)
+	return percentageChanged > float64(maxPercent)
 
-	p := uint32(bounds.Max.X * bounds.Max.Y)
+}
 
-	fmt.Printf("image1 channel sum: %d %d %d %d\n", colors_1[0]/p, colors_1[1]/p, colors_1[2]/p, colors_1[3]/p)
-	fmt.Printf("image2 channel sum: %d %d %d %d\n", colors_2[0]/p, colors_2[1]/p, colors_2[2]/p, colors_2[3]/p)
-
+func absDiff(a, b uint32) uint32 {
+	if a > b {
+		return a - b
+	}
+	return b - a
 }
